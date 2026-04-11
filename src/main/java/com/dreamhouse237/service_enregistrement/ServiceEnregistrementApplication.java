61c34eeac31734667ad9d8b1104ef79b96c22ba3
HeilTchambaNana <htchamba124@gmail.com>
2026-04-11 20:10:10 +0100

    First commit

---
+++ src/main/java/com/dreamhouse237/service_enregistrement/ServiceEnregistrementApplication.java
@@ -0,0 +1,15 @@
+package com.dreamhouse237.service_enregistrement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceEnregistrementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceEnregistrementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamhouse237/service_enregistrement/ServiceEnregistrementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamhouse237.service_enregistrement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEnregistrementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
